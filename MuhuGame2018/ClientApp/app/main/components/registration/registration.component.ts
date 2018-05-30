6d8ada44f7c1b25f919be808fe0e3a651db4a009
Jiri Novak <jiri.novak@petriny.net>
2018-05-30 22:06:59 +0200

    Registration screen.

--- MuhuGame2018/ClientApp/app/main/components/registration/registration.component.ts
+++ MuhuGame2018/ClientApp/app/main/components/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, TemplateRef, ElementRef/*, AfterViewInit*/ } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertService, UserService } from '../../../shared/_services';
 import { Team } from '../../_models';
@@ -8,11 +8,9 @@ import { Team } from '../../_models';
     templateUrl: './registration.component.html',
     styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent/* implements AfterViewInit*/ {
+export class RegistrationComponent implements OnInit {
     model: Team = new Team();
     loading = false;
-
-    /*@ViewChild('emailForm') emailForm: ElementRef;*/
 
     constructor(
         private router: Router,
@@ -20,14 +18,11 @@ export class RegistrationComponent/* implements AfterViewInit*/ {
         private alertService: AlertService,
     ) { }
 
-    /*
-    ngAfterViewInit() {
-        this.emailForm.nativeElement.formValidation();
+    ngOnInit() {
     }
-    */
 
     register() {
-        //this.loading = true;
+        this.loading = true;
         // this.userService.create(this.model)
         //     .subscribe(
         //         data => {
